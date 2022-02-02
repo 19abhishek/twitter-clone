6823d4415c565ea1e1dfcb635c9dc1b642b333b1
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-02-02 16:17:22 +0530

    Sidebar component added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import Head from 'next/head'
+import Sidebar from '../components/Sidebar'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Twitter</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <main className="mx-auto flex min-h-screen max-w-[1500px] bg-black">
+        <Sidebar />
+      </main>
     </div>
   )
 }
